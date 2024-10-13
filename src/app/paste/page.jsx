f9516656f613d6040473c3dae730e0bbe8f4eb54
Mirzya <mirzadevofficial@gmail.com>
2024-10-13 06:40:42 -0700

    Fixed issues!!

--- src/app/paste/page.jsx
+++ src/app/paste/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { ScrollArea } from "@/components/ui/scroll-area";
